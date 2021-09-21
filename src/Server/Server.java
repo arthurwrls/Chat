249ea98b5b168fbbd27e3c79b158dc17b5d05365
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-09-21 22:07:11 +0300

    Starting to build ViewGuiServer class

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,10 +1,78 @@
 package Server;
 
+import Connection.Connection;
+
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
 
 public class Server {
 
     private ServerSocket serverSocket;
     private static ViewGuiServer gui;
-    private static volatile boolean isServerStart = false;//
+    private static ModelGuiServer model
+    private static volatile boolean isServerStart = false;//flag reflecting the state of the server started / stopped
+
+    //method witch start the server
+    protected void startServer(int port) {
+        try {
+            serverSocket = new ServerSocket(port);
+            isServerStart = true;
+            gui.refreshDialogWindowServer("Server is running.\n");
+        } catch (Exception e) {
+            gui.refreshDialogWindowServer("Can`t start the server.\n");
+        }
+    }
+
+    //method witch stops the server
+    protected void stopServer() {
+        try {
+            //if server socket don`t have a link and not started
+            if (serverSocket != null && !serverSocket.isClosed())
+                for (Map.Entry<String, Connection> user : model.getAllUsersChat().entrySet()) {
+                    user.getValue().close();
+                }
+            serverSocket.close();
+            model.getAllUsersChat().clear();
+            gui.refreshDialogWindowServer("Server was stopped\n");
+        }else gui.refreshDialogWindowServer("Server not running - nothing to stop!\n");
+    }catch(
+    Exception e)
+
+    {
+        gui.refreshDialogWindowServer("Can`t stop the server\n");
+    }
 }
+
+    //method in endless loop witch, server takes new socket connection from client
+    protected void acceptServer() {
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                new ServerThread(socket).start();
+            } catch (Exception e) {
+                gui.refreshDialogWindowServer("Server connection lost..\n");
+                break;
+            }
+        }
+
+    }
+
+    //start point for server application
+    public static void main(String[] args) {
+        Server server = new Server();
+        gui = new ViewGuiServer(server);
+        model = new ModelGuiServer();
+        gui.initFrameServer();
+    /*loop down bellow waiting true from flag isServerStart (when the server starts in the
+    startServer method switching to  true), after will start endless loop witch taking connection
+    from client in acceptServer method, whilst server not stop or an exception will be thrown
+     */
+        while (true) {
+            if (isServerStart) {
+                server.acceptServer();
+                isServerStart = false;
+            }
+        }
+    }
+
--- src/Server/ViewGuiServer.java
+++ src/Server/ViewGuiServer.java
@@ -1,4 +1,40 @@
 package Server;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 public class ViewGuiServer {
+
+    private JFrame frame = new JFrame("Server start");
+    private JTextArea dialogWindow = new JTextArea(10,40);
+    private JButton buttonStartServer = new JButton("Run the server");
+    private JButton buttonStopServer = new JButton("Stop the server");
+    private JPanel panelButtons = new JPanel();
+    private final Server server;
+
+    public ViewGuiServer(Server server) {
+        this.server = server;
+    }
+
+    //server application GUI initialization method
+    protected void initFrameServer() {
+        dialogWindow.setEditable(false);
+        dialogWindow.setLineWrap(true); // automatic line break in JTextArea
+        frame.add(panelButtons, BorderLayout.SOUTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null);  //at start, displays the window in the center of the screen
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        //class for handling the event when the Server app during window closing
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                server.stopServer();
+                System.exit(0);
+            }
+        });
+            frame.setVisible(true);
+    }
 }
