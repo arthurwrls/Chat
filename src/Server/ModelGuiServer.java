e604239087bbd7b0e2da7169a9fd9c49e828891d
arturwrls <82509041+arturwrls@users.noreply.github.com>
2021-09-21 14:44:43 +0300

    Starting to build Server class

--- src/Server/ModelGuiServer.java
+++ src/Server/ModelGuiServer.java
@@ -1,5 +1,24 @@
 package Server;
 
+import Connection.Connection;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelGuiServer {
 
+    //model stores a map with all connected clients; the key is the name of the client, the value is the connection object
+    private Map<String, Connection> allUsersChat = new HashMap<>();
+
+    protected Map<String, Connection> getAllUsersChat() {
+        return allUsersChat;
+    }
+
+    protected void addUser(String userName, Connection connection) {
+        allUsersChat.put(userName, connection);
+    }
+
+    protected void removeUser(String userName) {
+        allUsersChat.remove(userName);
+    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,10 @@
 package Server;
 
+import java.net.ServerSocket;
+
 public class Server {
+
+    private ServerSocket serverSocket;
+    private static ViewGuiServer gui;
+    private static volatile boolean isServerStart = false;//
 }
